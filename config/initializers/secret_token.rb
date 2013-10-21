6bfebcfc46c2ba49adbc25f56771677bea6a5f62
codemonkey3045 <catia3045@gmail.com>
2013-10-21 14:54:21 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catia::Application.config.secret_key_base = '2fd4a73d95825354791547dd4f5097508c7331ce205ac9779bd23d53ff479976c089ccd9b46d95a4edc7f132f608217ea8ee919f1bf0a5eaf246c924f2803302'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Catia::Application.config.session_store :cookie_store, key: '_catia_session'
